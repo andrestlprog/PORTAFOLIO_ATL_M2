a6ffb165c02e487c7f4fd684fe09d59388100e52
Andres Torres Leiton <andrestorresleiton@gmail.com>
2024-08-24 20:15:56 -0400

    Portafolio ATL

---
+++ assets/js/script.js
@@ -0,0 +1,13 @@
+function Familia(apodo, edad, nombre){
+this.apodo=apodo;
+this.edad=edad;
+this.nombre=nombre;
+}
+
+var miFamilia1 = new Familia("Leonax", 13, "Leon"); 
+var miFamilia2 = new Familia("Frankiguagui", 2, "Franco");
+
+console.log(miFamilia1);
+console.log(miFamilia2);
+
+
